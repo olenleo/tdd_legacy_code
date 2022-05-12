7a19518a7b7a53eaae712f492748249819591685
Leo Niemi <niemi.leo@gmail.com>
2022-05-12 09:12:35 +0300

    Refactor tests

--- test/test_gilded_rose.js
+++ test/test_gilded_rose.js
@@ -2,21 +2,80 @@ var { expect } = require("chai");
 var { Shop, Item } = require("../src/gilded_rose.js");
 
 describe("Gilded Rose", function () {
-  it("should foo", function () {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).to.equal("foo");
-  });
-});
-
-describe("Inventory operations of non-special items:", () => {
   let gildedRose;
+  let inventory;
   let specialItems = [
     [new Item("Aged Brie", 20, 50)],
     [new Item("Backstage passes to a TAFKAL80ETC concert", 10, 50)],
     [new Item("Sulfuras, Hand of Ragnaros", 5, 50)]
   ]
   
+
+
+    it("should foo", function () {
+      const gildedRose = new Shop([new Item("foo", 0, 0)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].name).to.equal("foo");
+    });
+
+
+  describe("Inventory operations of non-special items:", () => {
+
+  
+
+    beforeEach(() => {
+      let averageItems =  [
+        new Item("Rare Sash", 5, 50),
+        new Item("Wings of Spit", 10, 100),
+        new Item("Low Quality Potion", 20, 1)
+      ]
+      
+      gildedRose = new Shop(averageItems);
+      inventory = gildedRose.items;
+      //printItems()
+    });
+    describe("Quality changes with update()", () => {
+      it("Quality of item decreases by one when updated if quality > 0", () => {
+        const currentQuality = inventory[0].quality
+        gildedRose.updateQuality()
+        expect(inventory[0].quality).to.equal(currentQuality - 1);
+      })
+
+      it("Quality can not reach below 0", () => {
+        updateTimes(5)
+        expect(inventory[2].quality).to.equal(0);
+      })
+
+      it("Quality of item A decreases even if item B reaches 0", () => {
+        updateTimes(5)
+        expect(inventory[0].quality).to.equal(45);
+      })
+  });
+    describe("sellIn (days to sell) changes with update()", () => {
+      it("Item sellIn decreases when updated", () => {
+        const currentSellIn = inventory[0].sellIn
+        updateTimes(1)
+        expect(inventory[0].sellIn).to.equal(currentSellIn - 1);
+      })
+      it("Item sellIn can be negative", () => {
+        const currentSellIn = inventory[0].sellIn;
+        updateTimes(6)
+        expect(inventory[0].sellIn).to.equal(currentSellIn - 6);
+      })
+      it("update() decreases sellIn of all items", () => {
+        updateTimes(5)
+      })
+    });
+    describe("Quality and sellIn affect each other:", () => {
+
+      it("Quality decreases at double speed if sellIn is negative", () => {
+        updateTimes(10)
+        expect(inventory[0].quality).to.equal(35)
+        updateTimes(1)
+        expect(inventory[0].quality).to.equal(33)
+      })
+    });
+  });
   function printItems() {
     console.log('\nItems\n______________________________________________________________')
     
@@ -27,38 +86,7 @@ describe("Inventory operations of non-special items:", () => {
   }
   function updateTimes(n) {
     for (let i = 0; i < n; i++) {
-      console.log('Update ', i + 1)
       gildedRose.updateQuality()
     }
   }
-
-  beforeEach(() => {
-    let averageItems =  [
-      new Item("Rare Sash", 5, 50),
-      new Item("Wings of Spit", 10, 100),
-      new Item("Low Quality Potion", 20, 1)
-    ]
-    gildedRose = new Shop(averageItems);
-    console.log('Initial items:')
-    printItems()
-  });
-
-  it("Quality of item decreases by one when updated if quality > 0", () => {
-    const quality = gildedRose.items[0].quality
-    gildedRose.updateQuality()
-    expect(gildedRose.items[0].quality).to.equal(quality - 1);
-  })
-
-  it("Quality can not reach below 0", () => {
-    updateTimes(5)
-    expect(gildedRose.items[2].quality).to.equal(0);
-  })
-
-  it("Quality of item A decreases even if item B reaches 0", () => {
-    updateTimes(5)
-    expect(gildedRose.items[0].quality).to.equal(45);
-  })
-
-
-
-})
+});
