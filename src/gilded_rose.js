02c09545a8ce3680b1dafb77df2c79a5aadcf6cd
Leo Niemi <niemi.leo@gmail.com>
2022-05-19 13:05:43 +0300

    Conditional to method

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -60,8 +60,11 @@ doThing( item ) {
   }
 */
 reduceQuality( indexOfItem ) {
-  if (this.items[indexOfItem].name != "Sulfuras, Hand of Ragnaros") {
-    this.items[indexOfItem].quality--;
+  if (this.items[indexOfItem].name === "Sulfuras, Hand of Ragnaros") {
+    return;
+  }
+  if (this.items[indexOfItem].quality - 1 >= 0) {
+  this.items[indexOfItem].quality--;
   }
 }
 
@@ -86,9 +89,7 @@ increaseQuality( indexOfItem ) {
   updateQuality() {
     for (var i = 0; i < this.items.length; i++) {
       if (this.items[i].name != "Aged Brie" && this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-        if (this.items[i].quality > 0) {
             this.reduceQuality(i)
-        }
       } else {
           this.increaseQuality(i)
           if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
@@ -104,9 +105,7 @@ increaseQuality( indexOfItem ) {
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != "Aged Brie") {
           if (this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-            if (this.items[i].quality > 0) {
                 this.reduceQuality( i ) 
-            }
           } else {
             this.items[i].quality = 0; // Backstage pass is expired
           }
