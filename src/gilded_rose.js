7f336e6cd32d68b192e5e52db27b0d0c1db767f6
Leo Niemi <niemi.leo@gmail.com>
2022-05-19 12:44:00 +0300

    Reduce quality and SellIn methods track unique item

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -60,11 +60,15 @@ doThing( item ) {
   }
 */
 reduceQuality( indexOfItem ) {
+  if (this.items[indexOfItem].name != "Sulfuras, Hand of Ragnaros") {
     this.items[indexOfItem].quality--;
+  }
 }
 
 reduceSellIn( indexOfItem ) {
+  if (this.items[indexOfItem].name != "Sulfuras, Hand of Ragnaros") {
   this.items[indexOfItem].sellIn--;
+  }
 }
 
 
@@ -94,16 +98,12 @@ reduceSellIn( indexOfItem ) {
           }
         }
       }
-      if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-        this.reduceSellIn(i)
-      }
+      this.reduceSellIn(i)
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != "Aged Brie") {
           if (this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
             if (this.items[i].quality > 0) {
-              if (this.items[i].name != "Sulfuras, Hand of Ragnaros") { 
                 this.reduceQuality( i ) 
-              }
             }
           } else {
             this.items[i].quality = 0;
